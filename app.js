64c50258e064aa53c7e23b5710494573fa4a240b
towen <terry@iknowmac.com>
2015-12-07 12:24:25 -0700

    added support for morris.js charts

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function(){
 
-    var app = angular.module('city-data', ['ngResource','ngRoute']);
+    var app = angular.module('city-data', ['ngResource','ngRoute','angular.morris-chart']);
     
     app.factory('Energy', function ($resource) {
         return $resource("https://data.lacity.org/api/views/rijp-9dwj/rows.json");
@@ -11,76 +11,41 @@
     });
     
     app.controller("EnergyCtrl", ['$scope','Energy',function($scope, Energy) {
-        $scope.sortType         = 'zip';  // set the default sort type
-        $scope.sortReverse      = false;        // set the default sort order
-        $scope.searchOrders     = '';           // set the default search/filter term
+        $scope.sortType         = 'zip';    // set the default sort type
+        $scope.sortReverse      = false;    // set the default sort order
+        $scope.searchOrders     = '';       // set the default search/filter term
         Energy.get(function(data) {
-            $scope.data = data.data;
-            $scope.meta = data.meta;
+            zipSet=[];
+            dataSet=[];
+            for (var i=0; i < data.data.length; i++){
+                dataSet.push(data.data[i].slice(8,16));
+                zipSet.push(data.data[i][16][0].slice(42,47));
+                dataSet[i].unshift(zipSet[i]);
+            }
+            
+            function toObject(names, values) {
+                var result = {};
+                for (var i = 0; i < names.length; i++)
+                    result[names[i]] = values[i];
+                return result;
+            }
+            
+            $scope.columns = data.meta.view.columns.slice(8,16);
+            $scope.columns.unshift({"name":"Zip Codes"});
+            $scope.data=dataSet;
+            $scope.meta=data.meta;
         });
     }]);
-    
+
     app.controller("WaterCtrl", ['$scope','Water',function($scope, Water) {
-        $scope.sortType         = 'zip';  // set the default sort type
-        $scope.sortReverse      = false;        // set the default sort order
-        $scope.searchOrders     = '';           // set the default search/filter term
+        $scope.sortType         = 'zip';    // set the default sort type
+        $scope.sortReverse      = false;    // set the default sort order
+        $scope.searchOrders     = '';       // set the default search/filter term
         Water.get(function(data) {
             $scope.data = data.data;
             $scope.meta = data.meta;
         });
     }]);
-    
-    $.plot(
-        $("#placeholder"),
-        [ [[1, 3],[2, 14.01],[3.5, 3.14]] ],
-        { yaxis: { max: 15 }, xaxis: { max: 15 } }
-    );
-    
-    var offset = 0;
-    plot();
-    function plot() {
-        var sin = [],
-            cos = [];
-        for (var i = 0; i < 12; i += 0.2) {
-            sin.push([i, Math.sin(i + offset)]);
-            cos.push([i, Math.cos(i + offset)]);
-        }
-
-        var options = {
-            series: {
-                lines: {
-                    show: true
-                },
-                points: {
-                    show: true
-                }
-            },
-            grid: {
-                hoverable: true //IMPORTANT! this is needed for tooltip to work
-            },
-            yaxis: {
-                min: -1.2,
-                max: 1.2
-            },
-            tooltip: true,
-            tooltipOpts: {
-                content: "'%s' of %x.1 is %y.4",
-                shifts: {
-                    x: -60,
-                    y: 25
-                }
-            }
-        };
-
-        var plotObj = $.plot($("#flot-line-chart"), [{
-                data: sin,
-                label: "sin(x)"
-            }, {
-                data: cos,
-                label: "cos(x)"
-            }],
-            options);
-    }
     
 })();
 
