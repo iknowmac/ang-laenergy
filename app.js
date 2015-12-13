9c990053c094edd93e22cb85a0974222abb2e9c6
Terry Owen <terry@iknowmac.com>
2015-12-13 14:13:37 -0700

    removed flot chart support & added Energy chart.

--- app.js
+++ app.js
@@ -10,31 +10,48 @@
         return $resource("https://data.lacity.org/api/views/hu2j-wciz/rows.json");
     });
     
-    app.controller("EnergyCtrl", ['$scope','Energy',function($scope, Energy) {
-        $scope.sortType         = 'zip';    // set the default sort type
-        $scope.sortReverse      = false;    // set the default sort order
-        $scope.searchOrders     = '';       // set the default search/filter term
+    app.controller("EnergyCtrl", ['$scope','$timeout','Energy',function($scope, $timeout, Energy) {
+        
+        $scope.sortType     = 'zip';    // set the default sort type
+        $scope.sortReverse  = false;    // set the default sort order
+        $scope.searchOrders = '';       // set the default search/filter term
+        
         Energy.get(function(data) {
-            zipSet=[];
-            dataSet=[];
+            aSet=[],bSet=[],dataSet=[],columnSet=[];
             for (var i=0; i < data.data.length; i++){
-                dataSet.push(data.data[i].slice(8,16));
-                zipSet.push(data.data[i][16][0].slice(42,47));
-                dataSet[i].unshift(zipSet[i]);
+                bSet.push(data.data[i].slice(8,16));
+                aSet.push(data.data[i][16][0].slice(42,47));
+                dataSet.push({zip:aSet[i],data:bSet[i]});
+            }
+            for (var i=0; i < data.meta.view.columns.slice(8,16).length; i++){
+                columnSet.push({name: data.meta.view.columns.slice(8,16)[i].name});
+            }
+            columnSet.unshift({"name":"Zip Codes"});
+            chartData=[]
+            for (var i=0; i < data.meta.view.columns.slice(8,16).length; i++){
+                col=data.meta.view.columns.slice(8,16)[i];
+                chartData.push({
+                    label: col.name,
+                    low: Math.round(col.cachedContents.smallest),
+                    average: Math.round(col.cachedContents.average),
+                    high: Math.round(col.cachedContents.largest)
+                });
             }
             
-            function toObject(names, values) {
-                var result = {};
-                for (var i = 0; i < names.length; i++)
-                    result[names[i]] = values[i];
-                return result;
+            $scope.ngGridFIx = function() {
+                $(window).resize();
             }
             
-            $scope.columns = data.meta.view.columns.slice(8,16);
-            $scope.columns.unshift({"name":"Zip Codes"});
-            $scope.data=dataSet;
-            $scope.meta=data.meta;
+            $scope.metaData         = data.meta;
+            $scope.tableColumns     = columnSet;
+            $scope.tableData        = dataSet;
+            
+            $timeout(function () {
+                $scope.chartData = chartData;
+            }, 3000);
         });
+        
+        
     }]);
 
     app.controller("WaterCtrl", ['$scope','Water',function($scope, Water) {
