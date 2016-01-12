eebded1c72a75280333ae67cc561c8d7abd1edc3
Terry Owen <terry@iknowmac.com>
2016-01-11 18:32:10 -0700

    cleaned up some Angular code.

--- app.js
+++ app.js
@@ -17,16 +17,19 @@
         $scope.searchOrders = '';       // set the default search/filter term
         
         Energy.get(function(data) {
+            
             aSet=[],bSet=[],dataSet=[],columnSet=[];
             for (var i=0; i < data.data.length; i++){
                 bSet.push(data.data[i].slice(8,16));
                 aSet.push(data.data[i][16][0].slice(42,47));
                 dataSet.push({zip:aSet[i],data:bSet[i]});
-            }
+            } // Pull out only the data needed fro the dataSet.
+            
             for (var i=0; i < data.meta.view.columns.slice(8,16).length; i++){
                 columnSet.push({name: data.meta.view.columns.slice(8,16)[i].name});
-            }
-            columnSet.unshift({"name":"Zip Codes"});
+            } // Pull out only the column headers needed for the dataSet.
+            columnSet.unshift({"name":"Zip Codes"}); // Prepend the "Zip Codes" header.
+            
             chartData=[]
             for (var i=0; i < data.meta.view.columns.slice(8,16).length; i++){
                 col=data.meta.view.columns.slice(8,16)[i];
@@ -36,21 +39,18 @@
                     average: Math.round(col.cachedContents.average),
                     high: Math.round(col.cachedContents.largest)
                 });
-            }
+            } // Need to present the chart data in a different format for Morris.
             
-            $scope.ngGridFIx = function() {
-                $(window).resize();
-            }
+            // Still need some work here and above on the chart timing issues.
+            $timeout(function () {
+                $scope.chartData = chartData;
+            }, 3000);
             
             $scope.metaData         = data.meta;
             $scope.tableColumns     = columnSet;
             $scope.tableData        = dataSet;
-            
-            $timeout(function () {
-                $scope.chartData = chartData;
-            }, 3000);
+                        
         });
-        
         
     }]);
 
